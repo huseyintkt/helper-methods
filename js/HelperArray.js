a9ca455be774a952e5ab6b77836c99ad8867e938
= <huseyintkt41@gmail.com>
2020-05-26 21:09:13 +0200

    biggestItemOfArray, smallestItemOfArray methods added to HelperArray

--- js/HelperArray.js
+++ js/HelperArray.js
@@ -5,10 +5,26 @@ class HelperArray{
      * This method, calculates the sum of the elements of the array sent as parameters.
      * @param {Array} pArray 
      */
-    sumOfArrayItems(pArray) {
+    static sumOfArrayItems(pArray) {
         let result = pArray.reduce((total, value) => {
             return total + value;
         });
         return result;
+    }
+
+    /**
+     * Bu fonksiyon, parametre olarak gönderilen dizinin en büyük elemanını bulur.
+     * 1. Yöntem
+     */
+    static biggestItemOfArray(pArray) {
+        return Math.max.apply(null, pArray);
+    }
+
+    /**
+     * Bu fonksiyon, parametre olarak gönderilen dizinin en küçük elemanını bulur.
+     * 1. Yöntem
+     */
+    static smallestItemOfArray(pArray) {
+        return Math.min.apply(null, pArray);
     }
 }
