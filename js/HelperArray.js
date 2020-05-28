31bf71fa720709989f5ddfad4831acc5882cee9c
= <huseyintkt41@gmail.com>
2020-05-28 11:10:48 +0200

    biggestItemOfArray2, smallestItemOfArray2 methods added to HelperArray

--- js/HelperArray.js
+++ js/HelperArray.js
@@ -27,4 +27,34 @@ class HelperArray{
     static smallestItemOfArray(pArray) {
         return Math.min.apply(null, pArray);
     }
+
+    /**
+     * Bu fonksiyon, parametre olarak gönderilen dizinin en büyük elemanını bulur.
+     * 2. Yöntem
+     */
+    static biggestItemOfArray2(pArray) {
+        var arrayLength = pArray.length;
+        var maxItem = -Infinity;
+        while (arrayLength--) {
+        if (pArray[arrayLength] > maxItem) {
+            maxItem = pArray[arrayLength];
+        }
+        }
+        return maxItem;
+    }
+
+    /**
+     * Bu fonksiyon, parametre olarak gönderilen dizinin en küçük elemanını bulur.
+     * 2. Yöntem
+     */
+    static smallestItemOfArray2(pArray) {
+        var arrayLength = pArray.length;
+        var minItem = Infinity;
+        while (arrayLength--) {
+        if (pArray[arrayLength] < minItem) {
+            minItem = pArray[arrayLength];
+        }
+        }
+        return minItem;
+    }
 }
