3d918cd8bedb56a148e65cceef204857f057824b
= <huseyintkt41@gmail.com>
2020-05-31 11:49:18 +0200

    averageOfArrayItems method added to HelperArray

--- js/HelperArray.js
+++ js/HelperArray.js
@@ -57,4 +57,42 @@ class HelperArray{
         }
         return minItem;
     }
+
+
+    /**
+     * Bu fonksiyon, parametre olarak gönderilen dizinin ortalamasını hesaplar.
+     */
+    static averageOfArrayItems(array) {
+        let total = array.reduce((total, value) => {
+            return total + value;
+        });
+        return total / array.length;
+    }
+
+
+/**
+    let points = [40, 100, 1, 5, 25, 10];
+    points.sort(function(a, b){return a - b});
+
+    var points = [40, 100, 1, 5, 25, 10];
+    points.sort(function(a, b){return b - a});
+
+    var cars = [
+        {type:"Volvo", year:2016},
+        {type:"Saab", year:2001},
+        {type:"BMW", year:2010}
+    ];
+
+    cars.sort(function(a, b){return a.year - b.year});
+
+    cars.sort(function(a, b){
+        var x = a.type.toLowerCase();
+        var y = b.type.toLowerCase();
+        if (x < y) {return -1;}
+        if (x > y) {return 1;}
+        return 0;
+    });
+
+
+ */
 }
