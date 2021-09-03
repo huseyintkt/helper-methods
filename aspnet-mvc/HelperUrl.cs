a1f2694ff02ec7e65e23344cd62787c6d764db54
= <huseyintkt41@gmail.com>
2021-09-03 23:44:59 +0200

    aspnet mvc edit url method added

---
+++ aspnet-mvc/HelperUrl.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Helpers
+{
+    public class HelperUrl
+    {
+        public static string EditUrl(string url)
+        {
+            if (url == "" || url == null) { return string.Empty; }
+
+            url = url.Replace(".", "");
+            url = url.Replace(":", "");
+            url = url.Replace(";", "");
+            url = url.Replace(",", "");
+            url = url.Replace(" ", "-");
+            url = url.Replace("!", "");
+            url = url.Replace("(", "");
+            url = url.Replace(")", "");
+            url = url.Replace("'", "'");
+            url = url.Replace("ğ", "g");
+            url = url.Replace("Ğ", "G");
+            url = url.Replace("ı", "i");
+            url = url.Replace("I", "i");
+            url = url.Replace("ç", "c");
+            url = url.Replace("Ç", "C");
+            url = url.Replace("ö", "o");
+            url = url.Replace("Ö", "O");
+            url = url.Replace("ü", "u");
+            url = url.Replace("Ü", "U");
+            url = url.Replace("ş", "s");
+            url = url.Replace("Ş", "S");
+            url = url.Replace("`", "");
+            url = url.Replace("=", "");
+            url = url.Replace("&", "");
+            url = url.Replace("%", "");
+            url = url.Replace("#", "");
+            url = url.Replace("<", "");
+            url = url.Replace(">", "");
+            url = url.Replace("*", "");
+            url = url.Replace("?", "");
+            url = url.Replace("+", "-");
+            url = url.Replace("\"", "-");
+            url = url.Replace("»", "-");
+            url = url.Replace("|", "-");
+            url = url.Replace("^", "");
+            return url;
+        }
+    }
+}
